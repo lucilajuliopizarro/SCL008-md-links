ee5398e2473757e2688250096f8a9944174a8812
lucitajulio <lucitajulio@hotmail.com>
2019-04-17 01:31:13 -0400

    pasando node index.js en terminal

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
- #!/usr/bin/env node 
+#!/usr/bin/env node 
 
 //con esto de arriba identifico que voy a trabajar con node
-  const mdLinks = require('./SCL008-md-links');// para el require no es necesario poner la extencion .js 
+  const mdLinks = require('./md-links.js');// para el require no es necesario poner la extencion .js 
   console.log(mdLinks.suma(2,2));
   console.log(mdLinks.multi(2,2));
 
