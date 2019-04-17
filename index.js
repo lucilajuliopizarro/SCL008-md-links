f8dac28b97e35463a9b4e38a67b1df9796ef85c6
lucitajulio <lucitajulio@hotmail.com>
2019-04-17 00:13:30 -0400

    importando y exportando

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports = () => {
-  // ...
-};
+ #!/usr/bin/env node 
+
+//con esto de arriba identifico que voy a trabajar con node
+  const mdLinks = require('./SCL008-md-links');// para el require no es necesario poner la extencion .js 
+  console.log(mdLinks.suma(2,2));
+  console.log(mdLinks.multi(2,2));
+
+  
