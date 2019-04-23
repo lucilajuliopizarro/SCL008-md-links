5c9181348d841264f706d269d939397bde08c646
lucitajulio <lucitajulio@hotmail.com>
2019-04-23 16:08:02 -0400

    extrayendo archivos mad con node

--- md-links.js
+++ md-links.js
@@ -1,21 +1,23 @@
-const fs = require('fs');
-const fetch = require('node-fetch');
-const command = process.argv[2]//te toma desde la posicion dos 
-const markdownLinkExtractor = require('markdown-link-extractor');
-const markdown = fs.readFileSync(command).toString();// leyendo el command , antes era el readme
-const links = markdownLinkExtractor(markdown);//aqui extrae lo deja en lick 
 
-links.forEach(function (element) {// lo recorre  
-fetch(element).then((res)=>{ // fetch toma el elemnto que toma
-    console.log(res.url + " " + res.status + " " + res.statusText); // te toma la url un espacio el numero 200 y si esta ok el lick
-})
-.catch (error=>{
+//const fs = require('fs');
+//const fetch = require('node-fetch');
+//const command = process.argv[2]//te toma desde la posicion dos 
+//const markdownLinkExtractor = require('markdown-link-extractor');
+//const markdown = fs.readFileSync(command).toString();// leyendo el command , antes era el readme
+//const links = markdownLinkExtractor(markdown);//aqui extrae lo deja en linck 
 
- console.log(error.message);
-})
-});
+//links.forEach(function (element) {// lo recorre  
+//fetch(element).then((res)=>{ // fetch toma el elemnto que toma
+    //console.log(res.url + " " + res.status + " " + res.statusText); // te toma la url un espacio el numero 200 y si esta ok el lick
+//})
+//.catch (error=>{
 
-exports.links = links;
+// console.log(error.message);
+//})
+//});
+
+//exports.links = links;
+
 
 //const fs = require('fs');
 //const markdownLinkChecker = require('broken-link-checker');
@@ -28,4 +30,28 @@ exports.links = links;
  //console.log(link);
 //});
 
-  
+//extrae archivos md 
+const fs = require('fs');
+const path = require('path');
+const dir = "C:\\Users\\Luci\\Desktop\\laboratoria";
+// Check write permission
+/* try {
+    fs.accessSync(dir, fs.constants.W_OK);
+    console.log('can write %s', dir);
+}
+catch (err) {
+    console.log("%s doesn't exist", dir);
+} */
+
+
+let firstArray = [];
+let answer = fs.readdir(dir, (err, files) => {
+    console.log("dir:", dir);
+    console.log("files:", files);
+    files.forEach(file => {
+        if (path.extname(file) == '.md') {
+            firstArray.push(file);
+        };
+    });
+    console.log(firstArray);
+});
