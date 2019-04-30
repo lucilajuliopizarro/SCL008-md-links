eabb3b59f506a554e3bc8347b8f0dde41a2aa038
lucitajulio <lucitajulio@hotmail.com>
2019-04-29 23:48:13 -0400

    incluyo README

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -6,6 +6,6 @@ describe('mdLinks', () => {
   it('Debería retornar los 3 links que se encuentran en pruebatest.md', () => {
     expect(mdLinks.readFile('pruebatest.md')).resolves.toEqual(['https://es.wikipedia.org/wiki/Markdown', 'https://nodejs.org/','https://nodejs.org/docs/latest-v0.10.x/api/modules.html']);
   });
-
+ 
 });
 
